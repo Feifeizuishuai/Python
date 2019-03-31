eb2e4cbdc2eef5e5eeb68de7f64b9b5588f66d73
A fei <149669035@qq.com>
2019-04-01 01:07:18 +0800

    update 31/3
    add data for exercise
    edited some scripts about Linear regression model

--- Scripts/ex5_2_2.py
+++ Scripts/ex5_2_2.py
@@ -4,31 +4,38 @@ from matplotlib.pyplot import figure, plot, xlabel, ylabel, legend, show
 import sklearn.linear_model as lm
 import numpy as np
 
+from sklearn.preprocessing import PolynomialFeatures
+
+
 # Use dataset as in the previous exercise
 N = 100
-X = np.array(range(N)).reshape(-1,1)
+X = np.random.uniform(-3, 3, size=N).reshape(-1,1)
 eps_mean, eps_std = 0, 0.1
 eps = np.array(eps_std*np.random.randn(N) + eps_mean).reshape(-1,1)
 w0 = -0.5
 w1 = 0.01
-y = w0 + w1*X + eps
+w2 = 0.05
+y = w0 + w1*X + w2*X**2 + eps
 y_true = y - eps
 
+poly = PolynomialFeatures(degree=2)
+poly.fit(X)
+X2 = poly.transform(X)
+
 # Fit ordinary least squares regression model
-model = lm.LinearRegression(fit_intercept=True)
-model = model.fit(X,y)
+model = lm.LinearRegression(fit_intercept=False)
+model = model.fit(X2,y)
 # Compute model output:
-y_est = model.predict(X)
+y_est = model.predict(X2)
 # Or equivalently:
 #y_est = model.intercept_ + X @ model.coef_
 
 
 # Plot original data and the model output
 f = figure()
-
 plot(X,y,'.')
-plot(X,y_true,'-')
-plot(X,y_est,'-')
+plot(X,y_true,'*')
+plot(X,y_est,'.')
 xlabel('X'); ylabel('y')
 legend(['Training data', 'Data generator', 'Regression fit (model)'])
 
